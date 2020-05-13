8c0de4d75e7e7bd4354ae7c22317de4a04bf5fdf
SimranDhillon20 <46889015+SimranDhillon20@users.noreply.github.com>
2020-05-13 10:08:06 +0100

    Added Phone Class

---
+++ SimranDhillon_S00187359/Phone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimranDhillon_S00187359
+{
+    
+    class Phone
+    {
+       
+        public string Name { get; set; }
+
+        public float Price { get; set; }
+        public string OperatingSystem { get; set; }
+        public string OS_Image { get; set; }
+        public string Phone_Image { get; set; }
+    }
+}
