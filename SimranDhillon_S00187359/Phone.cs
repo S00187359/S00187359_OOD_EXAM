1127926931b04a9b03e4bab6cb64743f16c15ec1
SimranDhillon20 <46889015+SimranDhillon20@users.noreply.github.com>
2020-05-13 10:33:43 +0100

    Added IncreasePrice Method

--- SimranDhillon_S00187359/Phone.cs
+++ SimranDhillon_S00187359/Phone.cs
@@ -8,13 +8,24 @@ namespace SimranDhillon_S00187359
 {
     
     class Phone
-    {
-       
+    {     
         public string Name { get; set; }
-
         public float Price { get; set; }
         public string OperatingSystem { get; set; }
         public string OS_Image { get; set; }
         public string Phone_Image { get; set; }
+
+
+        public float IncreasePrice()
+        {
+            float percentage, price;
+            Console.WriteLine("Enter Percentage Increase For The Price : {0}",Price);
+            percentage = float.Parse(Console.ReadLine());
+            price = percentage * Price;
+            Price += price;
+            return Price;
+        }
     }
+
+   
 }
